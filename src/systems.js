003f8b9df1f0d81e5182a7241e08d7232a1a3929
LinkNavi <LinkNavi@morke.ru>
2026-01-02 15:48:27 -0600

    Delete src directory

--- src/systems.js
+++
@@ -1,7 +0,0 @@
-// move system
-function movementSystem(entity, { position, velocity }, dt) {
-  position.x += velocity.vx * dt;
-  position.y += velocity.vy * dt;
-}
-
-module.exports = { movementSystem };
